ba4c42965b1767318d4e68ca6caa427eb598bf68
liekebpsv <99262550+liekebpsv@users.noreply.github.com>
2022-02-11 14:02:55 +0100

    assignment 2 exercise 3 is af!

--- 8p361-project-imaging-master/code/assignment2exercise3.py
+++ 8p361-project-imaging-master/code/assignment2exercise3.py
@@ -62,9 +62,56 @@ X_test /= 255
 y_train = to_categorical(y_train, 10)
 y_val = to_categorical(y_val, 10)
 y_test = to_categorical(y_test, 10)
-#%%
-for ytr, yva, yte in zip(y_train, y_val, y_test):
-    print(ytr[0])
+#%% converting the 10D class matrices to 4D class matrices
+
+#creating memory to save the new classes
+y_train_4 = np.zeros((54000,4))
+y_val_4 = np.zeros((6000,4))
+y_test_4 = np.zeros((10000,4))
+
+#a for loop over the indices of y_train
+for i in range(y_train.shape[0]):
+    
+    ytr = y_train[i]
+   #filling the new classes for the training set
+    if ytr[1] ==1 or ytr[7]==1:
+        y_train_4[i][0] = 1
+    if ytr[0] ==1 or ytr[6]==1 or ytr[8]==1 or ytr[9]==1:
+        y_train_4[i][1] =1
+    if ytr[2]==1 or ytr[5]==1:
+        y_train_4[i][2]=1
+    if ytr[3]==1 or ytr[4]==1:
+        y_train_4[i][3]=1
+
+#a for loop over the indices of y_val
+for i in range(y_val.shape[0]):
+    
+    yva= y_val[i]
+
+    #filling in the new classes for the validation set
+    if yva[1] ==1 or yva[7]==1:
+        y_val_4[i][0] = 1
+    if yva[0] ==1 or yva[6]==1 or yva[8]==1 or yva[9]==1:
+        y_val_4[i][1] =1
+    if yva[2]==1 or yva[5]==1:
+        y_val_4[i][2]=1
+    if yva[3]==1 or yva[4]==1:
+        y_val_4[i][3]=1    
+        
+#a for loop over the indices of y_test
+for i in range(y_test.shape[0]):
+    
+    yte = y_test[i]
+
+    #filling in the new classes for the test set
+    if yte[1] ==1 or yte[7]==1:
+        y_test_4[i][0] = 1
+    if yte[0] ==1 or yte[6]==1 or yte[8]==1 or yte[9]==1:
+        y_test_4[i][1] =1
+    if yte[2]==1 or yte[5]==1:
+        y_test_4[i][2]=1
+    if yte[3]==1 or yte[4]==1:
+        y_test_4[i][3]=1        
 
 
 #%% Creating the models for exercise 1:
@@ -76,23 +123,25 @@ model = Sequential()
 model.add(Flatten(input_shape=(28,28,1))) 
 # fully connected layer with 64 neurons and ReLU nonlinearity
 model.add(Dense(64, activation='relu'))
+# fully connected layer with 64 neurons and ReLU nonlinearity
+model.add(Dense(64, activation='relu'))
 # output layer with 10 nodes (one for each class) and softmax nonlinearity
-model.add(Dense(10, activation='softmax')) 
+model.add(Dense(4, activation='softmax')) 
 
 # compile the model_1
 model.compile(loss='categorical_crossentropy', optimizer='sgd', metrics=['accuracy'])
 
 # use this variable to name your model
-model_name="model_1a"
+model_name="model_3"
 
 # create a way to monitor our model in Tensorboard
 tensorboard = TensorBoard("logs/" + model_name)
 
 # train the model
-model.fit(X_train, y_train, batch_size=32, epochs=20, verbose=1, validation_data=(X_val, y_val), callbacks=[tensorboard])
+model.fit(X_train, y_train_4, batch_size=32, epochs=20, verbose=1, validation_data=(X_val, y_val_4), callbacks=[tensorboard])
 
 
-score = model.evaluate(X_test, y_test, verbose=0)
+score = model.evaluate(X_test, y_test_4, verbose=0)
 
 
 print("Loss: ",score[0])
