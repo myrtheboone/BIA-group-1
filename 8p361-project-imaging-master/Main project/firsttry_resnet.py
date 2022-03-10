af2504c353ab5aa86a56bcd4a6a48f16c3ddf1c0
myrtheboone <myrthe.boone@gmail.com>
2022-03-10 19:04:58 +0100

    Added some additional remarks to literature study and first try with ResNet

---
+++ 8p361-project-imaging-master/Main project/firsttry_resnet.py
@@ -0,0 +1,106 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 10 17:33:15 2022
+
+@author: 20192024
+"""
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}   
+import tensorflow as tf
+
+import numpy as np
+
+
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.layers import Conv2D, MaxPool2D
+from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard
+import matplotlib.pyplot as plt
+
+from tensorflow.keras.applications import ResNet50
+from tensorflow.keras.layers import Dense, Flatten, GlobalAveragePooling2D
+from tensorflow.keras.optimizers import RMSprop
+
+from sklearn.metrics import roc_curve, auc
+
+
+IMAGE_SIZE = 96
+
+
+def get_pcam_generators(base_dir, train_batch_size=32, val_batch_size=32):
+
+     # dataset parameters
+     train_path = os.path.join(base_dir, 'train+val', 'train')
+     valid_path = os.path.join(base_dir, 'train+val', 'valid')
+
+
+     RESCALING_FACTOR = 1./255
+
+     # instantiate data generators
+     datagen = ImageDataGenerator(rescale=RESCALING_FACTOR)
+
+     train_gen = datagen.flow_from_directory(train_path,
+                                             target_size=(IMAGE_SIZE, IMAGE_SIZE),
+                                             batch_size=train_batch_size,
+                                             class_mode='binary')
+
+     val_gen = datagen.flow_from_directory(valid_path,
+                                             target_size=(IMAGE_SIZE, IMAGE_SIZE),
+                                             batch_size=val_batch_size,
+                                             class_mode='binary', shuffle = False) 
+
+     return train_gen, val_gen
+
+train_gen, val_gen = get_pcam_generators('C:/Users//20192024//Documents//Project_BIA')
+
+
+
+base_model = Sequential()
+base_model.add(ResNet50(include_top=False, weights='imagenet', pooling='max'))
+base_model.add(Dense(1, activation='sigmoid'))
+
+base_model.compile(optimizer = RMSprop(lr=0.0001), loss = 'binary_crossentropy', metrics = ['acc'])
+
+train_steps = train_gen.n//train_gen.batch_size
+val_steps = val_gen.n//val_gen.batch_size
+
+history = base_model.fit(train_gen, steps_per_epoch=train_steps,
+                    validation_data=val_gen,
+                    validation_steps=val_steps,
+                    epochs=3)
+                    #callbacks=callbacks_list)
+
+
+#Plot ROC curves of ResNet
+
+val_prob = base_model.predict(val_gen)
+filenames=val_gen.filenames
+val_true_labels = []
+
+for i in filenames:
+    val_true_labels.append(int(i[0]))
+
+val_true_array = np.array(val_true_labels)
+
+val_prob_array = np.array(val_prob)
+
+val_true_array = val_true_array.reshape(16000,1)
+
+# Plotting the ROC curve
+
+fpr , tpr , thresholds = roc_curve(val_true_labels, val_prob)
+
+def plot_roc_curve(fpr,tpr): 
+  plt.plot(fpr,tpr) 
+  plt.axis([0,1,0,1]) 
+  plt.xlabel('False Positive Rate') 
+  plt.ylabel('True Positive Rate') 
+  plt.title('ROC curve - model with dense layers')
+  plt.show()    
+  
+plot_roc_curve (fpr,tpr) 
+auc_score = auc(fpr, tpr)
+
