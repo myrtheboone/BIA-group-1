2779c9a4f2d8d242ee9bea2394b2306306b9b611
Jimvmook <94608370+Jimvmook@users.noreply.github.com>
2022-03-21 16:45:03 +0100

    hoi

--- 8p361-project-imaging-master/Main project/DataPreprocessing.py
+++ 8p361-project-imaging-master/Main project/DataPreprocessing.py
@@ -7,20 +7,20 @@ Created on Thu Mar 17 15:37:27 2022
 # disable overly verbose tensorflow logging
 import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'  # or any {'0', '1', '2'}   
-import tensorflow as tf
+#import tensorflow as tf
 
-import numpy as np
+#import numpy as np
 
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Flatten, RandomFlip, RandomRotation
-from tensorflow.keras.layers import Conv2D, MaxPool2D
-from tensorflow.keras.optimizers import SGD
-from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard
+#from tensorflow.keras.models import Sequential
+#from tensorflow.keras.layers import Dense, Flatten, RandomFlip, RandomRotation
+#from tensorflow.keras.layers import Conv2D, MaxPool2D
+#from tensorflow.keras.optimizers import SGD
+#from tensorflow.keras.callbacks import ModelCheckpoint, TensorBoard
 import matplotlib.pyplot as plt
 # unused for now, to be used for ROC analysis
-from sklearn.metrics import roc_curve, auc
-import cv2
+#from sklearn.metrics import roc_curve, auc
+#import cv2
 
 # the size of the images in the PCAM dataset
 IMAGE_SIZE = 96
@@ -36,30 +36,44 @@ def get_pcam_generators(base_dir, train_batch_size=32, val_batch_size=32):
 
       # instantiate data generators
       datagen = ImageDataGenerator(rescale=RESCALING_FACTOR,rotation_range=30,horizontal_flip=True, vertical_flip=True, fill_mode='nearest')
-
-      train_gen = datagen.flow_from_directory(train_path,
+      train_gen1 = datagen.flow_from_directory(train_path,
+                                               target_size=(IMAGE_SIZE, IMAGE_SIZE),
+                                               batch_size=train_batch_size,
+                                               class_mode='binary')
+      
+      train_gen2 = datagen.flow_from_directory(train_path,
+                                               target_size=(IMAGE_SIZE, IMAGE_SIZE),
+                                               batch_size=train_batch_size,
+                                               class_mode='binary')
+      
+      train_gen3 = datagen.flow_from_directory(train_path,
                                                target_size=(IMAGE_SIZE, IMAGE_SIZE),
                                                batch_size=train_batch_size,
                                                class_mode='binary')
 
-      val_gen = datagen.flow_from_directory(valid_path,
-                                                 target_size=(IMAGE_SIZE, IMAGE_SIZE),
-                                                 batch_size=val_batch_size,
-                                                 class_mode='binary', shuffle = False) 
+      
+      train_gen4 = datagen.flow_from_directory(train_path,
+                                               target_size=(IMAGE_SIZE, IMAGE_SIZE),
+                                               batch_size=train_batch_size,
+                                               class_mode='binary')
+         
+      
+      
        # NB: first SHUFFLE = FALSE was not specified in the val_gen definition
        # This was however specified in the code in the jupyter notebook
        # This was the reason for the odd ROC curves
 
 
       
-      return train_gen
+      return train_gen1, train_gen2, train_gen3, train_gen4
  
 # get the data generators
-train_gen = get_pcam_generators(r'C:\Users\20191819\Documents\school\2021,2022\Q3\ProjectBIA\data')
+[train_gen1, train_gen2, train_gen3, train_gen4] = get_pcam_generators(r'C:\Users\20192823\Documents\3 jaar\Kwartiel 3\BIA')  #Jim
+#train_gen = get_pcam_generators(r'C:\Users\2019\Documents\3 jaar\kwartiel 3\Q3\ProjectBIA\data') #Lieke
 
 
 # #import an image to test the code
-image = cv2.imread(r'C:\Users\20191819\Documents\school\2021,2022\Q3\ProjectBIA\data\train+val\train\0\19f679632eefea5cd8009a7590da89852fc23054.jpg')
+#image = cv2.imread(r'C:\Users\20191819\Documents\school\2021,2022\Q3\ProjectBIA\data\train+val\train\0\19f679632eefea5cd8009a7590da89852fc23054.jpg')
 
 iter_size = 4
 batch_size = 32
@@ -68,5 +82,11 @@ batch_size = 32
 for j in range(batch_size):
     fig, ax = plt.subplots(nrows=1, ncols=iter_size, figsize=(15,15))
     for i in range(iter_size):
-        img = next(train_gen)[0][j]
+        img = next(train_gen1)[0][j]
+        ax[i].imshow(img)
+        img = next(train_gen2)[0][j]
+        ax[i].imshow(img)
+        img = next(train_gen3)[0][j]
+        ax[i].imshow(img)
+        img = next(train_gen4)[0][j]
         ax[i].imshow(img)
