a1d2031ea9300425fcac22fdba8f12cb23a55516
Jimvmook <94608370+Jimvmook@users.noreply.github.com>
2022-03-22 11:29:24 +0100

    auw

--- 8p361-project-imaging-master/Main project/DataPreprocessing.py
+++ 8p361-project-imaging-master/Main project/DataPreprocessing.py
@@ -25,11 +25,11 @@ import matplotlib.pyplot as plt
 # the size of the images in the PCAM dataset
 IMAGE_SIZE = 96
 
-def get_pcam_generators(base_dir, train_batch_size=32, val_batch_size=32):
+def get_pcam_generators(base_dir, train_batch_size, val_batch_size=32):
 
       # dataset parameters
       train_path = os.path.join(base_dir, 'train+val', 'train')
-      valid_path = os.path.join(base_dir, 'train+val', 'valid')
+      #valid_path = os.path.join(base_dir, 'train+val', 'valid')
 
 
       RESCALING_FACTOR = 1./255
@@ -40,18 +40,18 @@ def get_pcam_generators(base_dir, train_batch_size=32, val_batch_size=32):
                                                target_size=(IMAGE_SIZE, IMAGE_SIZE),
                                                batch_size=train_batch_size,
                                                class_mode='binary')
-      
+      datagen = ImageDataGenerator(rescale=RESCALING_FACTOR,rotation_range=30,horizontal_flip=True, vertical_flip=True, fill_mode='nearest')
       train_gen2 = datagen.flow_from_directory(train_path,
                                                target_size=(IMAGE_SIZE, IMAGE_SIZE),
                                                batch_size=train_batch_size,
                                                class_mode='binary')
-      
+      datagen = ImageDataGenerator(rescale=RESCALING_FACTOR,rotation_range=30,horizontal_flip=True, vertical_flip=True, fill_mode='nearest')
       train_gen3 = datagen.flow_from_directory(train_path,
                                                target_size=(IMAGE_SIZE, IMAGE_SIZE),
                                                batch_size=train_batch_size,
                                                class_mode='binary')
 
-      
+      datagen = ImageDataGenerator(rescale=RESCALING_FACTOR,rotation_range=30,horizontal_flip=True, vertical_flip=True, fill_mode='nearest')
       train_gen4 = datagen.flow_from_directory(train_path,
                                                target_size=(IMAGE_SIZE, IMAGE_SIZE),
                                                batch_size=train_batch_size,
@@ -66,27 +66,53 @@ def get_pcam_generators(base_dir, train_batch_size=32, val_batch_size=32):
 
       
       return train_gen1, train_gen2, train_gen3, train_gen4
+  
+    
+    
+def get_pcam_generators2(base_dir, train_batch_size, val_batch_size=32):
+
+      # dataset parameters
+      train_path = os.path.join(base_dir, 'train+val', 'train')
+      #valid_path = os.path.join(base_dir, 'train+val', 'valid')
+
+
+      RESCALING_FACTOR = 1./255
+
+      # instantiate data generators
+      datagen = ImageDataGenerator(rescale=RESCALING_FACTOR,rotation_range=30,horizontal_flip=True, vertical_flip=True, fill_mode='nearest')
+      train_gen1 = datagen.flow_from_directory(train_path,
+                                               target_size=(IMAGE_SIZE, IMAGE_SIZE),
+                                               batch_size=train_batch_size,
+                                               class_mode='binary')
+      
+      
+       # NB: first SHUFFLE = FALSE was not specified in the val_gen definition
+       # This was however specified in the code in the jupyter notebook
+       # This was the reason for the odd ROC curves
+
+
+      
+      return train_gen1
  
 # get the data generators
-[train_gen1, train_gen2, train_gen3, train_gen4] = get_pcam_generators(r'C:\Users\20192823\Documents\3 jaar\Kwartiel 3\BIA')  #Jim
-#train_gen = get_pcam_generators(r'C:\Users\2019\Documents\3 jaar\kwartiel 3\Q3\ProjectBIA\data') #Lieke
-
 
 # #import an image to test the code
 #image = cv2.imread(r'C:\Users\20191819\Documents\school\2021,2022\Q3\ProjectBIA\data\train+val\train\0\19f679632eefea5cd8009a7590da89852fc23054.jpg')
 
 iter_size = 4
-batch_size = 32
-
+batch_size = 5
+[train_gen1, train_gen2, train_gen3, train_gen4] = get_pcam_generators(r'C:\Users\20192823\Documents\3 jaar\Kwartiel 3\BIA', batch_size)  #Jim
+#train_gen1 = get_pcam_generators2(r'C:\Users\20192823\Documents\3 jaar\Kwartiel 3\BIA', batch_size)  #Jim
+#train_gen = get_pcam_generators(r'C:\Users\2019\Documents\3 jaar\kwartiel 3\Q3\ProjectBIA\data') #Lieke
 
 for j in range(batch_size):
     fig, ax = plt.subplots(nrows=1, ncols=iter_size, figsize=(15,15))
     for i in range(iter_size):
-        img = next(train_gen1)[0][j]
-        ax[i].imshow(img)
-        img = next(train_gen2)[0][j]
-        ax[i].imshow(img)
-        img = next(train_gen3)[0][j]
-        ax[i].imshow(img)
-        img = next(train_gen4)[0][j]
-        ax[i].imshow(img)
+        img = train_gen1[0][0][j]
+        ax[0].imshow(img)
+        img = train_gen2[0][0][j]
+        ax[1].imshow(img)
+        img = train_gen3[0][0][j]
+        ax[2].imshow(img)
+        img = train_gen4[0][0][j]
+        ax[3].imshow(img)
